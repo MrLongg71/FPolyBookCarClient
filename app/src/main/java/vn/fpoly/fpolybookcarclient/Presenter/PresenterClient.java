51fb3b2216d14ad2965c753c41cb2177df0bbab9
LongKhoa0706 <49479782+LongKhoa0706@users.noreply.github.com>
2019-09-06 22:16:23 +0700

    test"

--- app/src/main/java/vn/fpoly/fpolybookcarclient/Presenter/PresenterClient.java
+++ app/src/main/java/vn/fpoly/fpolybookcarclient/Presenter/PresenterClient.java
@@ -1,16 +1,5 @@
 package vn.fpoly.fpolybookcarclient.Presenter;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.PhoneAuthCredential;
-
-import java.util.concurrent.Executor;
-
-import vn.fpoly.fpolybookcarclient.Model.ModelClient;
 import vn.fpoly.fpolybookcarclient.View.Fragment.SignInFragment;
 import vn.fpoly.fpolybookcarclient.View.Interface.ViewClient;
 
