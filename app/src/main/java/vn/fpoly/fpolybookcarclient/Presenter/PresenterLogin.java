d87e02cb21f3d6973e71ba79ec4f527d223378b1
MrLongg71 <41661101+MrLongg71@users.noreply.github.com>
2019-09-08 22:38:04 +0700

    Login with email and verify code sms phone mapping mvp parter

--- app/src/main/java/vn/fpoly/fpolybookcarclient/Presenter/PresenterLogin.java
+++ app/src/main/java/vn/fpoly/fpolybookcarclient/Presenter/PresenterLogin.java
@@ -2,15 +2,15 @@ package vn.fpoly.fpolybookcarclient.Presenter;
 
 import vn.fpoly.fpolybookcarclient.Model.ObjectClass.Client;
 import vn.fpoly.fpolybookcarclient.View.Fragment.SignInFragment;
-import vn.fpoly.fpolybookcarclient.View.Interface.ViewClient;
+import vn.fpoly.fpolybookcarclient.View.Interface.ViewLogin;
 
 public class PresenterClient implements IPPresenterClient {
 
     Client modelClient;
-    ViewClient viewClient;
+    ViewLogin viewLogin;
     SignInFragment signInFragment;
-    public PresenterClient(ViewClient viewClient){
-        viewClient = viewClient;
+    public PresenterClient(ViewLogin viewLogin){
+        viewLogin = viewLogin;
 
     }
 
---
+++ app/src/main/java/vn/fpoly/fpolybookcarclient/View/Activity/LoginSMSActivity.java
@@ -0,0 +1,14 @@
+package vn.fpoly.fpolybookcarclient.View.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginSMSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sms);
+    }
+}
