b894eaa140c77393f94861bcabbc77eb6f1da0d3
MrLongg71 <41661101+MrLongg71@users.noreply.github.com>
2019-12-03 09:47:54 +0700

    Merge branch 'dev' of /Users/pro/ProjectAndroid/FPolyBookCarClient with conflicts.

--- functions/index.js
+++ functions/index.js
@@ -5,76 +5,86 @@ var admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 exports.sendNotification = functions.database
-
+<<<<<<< Updated upstream
   .ref("/notification/{notificationId}")
   .onWrite((change, context) => {
-
+    
     const idOrder = change.after.child("idOrder").val();
 
     const idDriver = change.after.child("idDriver").val();
 
     return admin.database().ref().child("Driver").child("Car").child(idDriver).child("token").once('value')
-      .then(function (snapshot) {
-        var token_id = snapshot.val();
+    .then(function(snapshot){
+      var token_id = snapshot.val();
 
 
-        console.log(token_id);
+      console.log(token_id);
 
 
-        var payload = {
-          data: {
-            idOrder: idOrder,
-            idDriver: idDriver
-          },
-          token: token_id
+      var payload = {
+        data: {
+          idOrder: idOrder,
+          idDriver: idDriver
+        },
+        token : token_id
+  
+=======
+  .ref("/article/{articleId}")
+  .onWrite((change, context) => {
+      
+    const author = change.after.child("author").val();
+    const title = change.after.child("title").val();
+    var iduser = change.after.child("iduser").val();
+
+    console.log(iduser)
+
+    var str1 = "Author is ";
+    var str = str1.concat(author);
+    console.log(str);
+    // var tokenRE = 'fvI0xGm3N84:APA91bHCwyzqO4pgJSgTjyjRzz34AhyJ-x_qYP0KevtbLfz3kVr28LtsInTf5D3AYLpeZ2Db99nGczLCM0X0ZN4xhvhlSyBaiHkGi1SEOQz8r0ay6VxBqq57x9ZHa3NkiJVPLGSuisZN';
+
+    return admin.database().ref().child("User").child(iduser).child("token").once('value')
+    .then(function(snapshot) {
+      // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+      // send thu di
+      var tokenRE = snapshot.val();
+
+      var strtoken = "Token is ";
+      var stre = strtoken.concat(tokenRE);
+      console.log(stre);
 
 
-            .ref("/article/{articleId}")
-            .onWrite((change, context) => {
+      var topic = "android";
+      var payload = {
+        data: {
+          title: author,
+          author: title
+        },
+        token :  tokenRE
+>>>>>>> Stashed changes
+      };
+      return admin
+        .messaging()
+        .send(payload)
+        .then(function(response) {
+          return console.log("Successfully sent message:", response);
+        })
+        .catch(function(error) {
+          return console.log("Error sending message:", error);
+<<<<<<< Updated upstream
+        });
 
-              const author = change.after.child("author").val();
-              const title = change.after.child("title").val();
-              var iduser = change.after.child("iduser").val();
 
-              console.log(iduser)
+    
+  });
+});
+=======
+        }); // send di
 
-              var str1 = "Author is ";
-              var str = str1.concat(author);
-              console.log(str);
-              // var tokenRE = 'fvI0xGm3N84:APA91bHCwyzqO4pgJSgTjyjRzz34AhyJ-x_qYP0KevtbLfz3kVr28LtsInTf5D3AYLpeZ2Db99nGczLCM0X0ZN4xhvhlSyBaiHkGi1SEOQz8r0ay6VxBqq57x9ZHa3NkiJVPLGSuisZN';
 
-              return admin.database().ref().child("User").child(iduser).child("token").once('value')
-                .then(function (snapshot) {
-                  // var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-                  // send thu di
-                  var tokenRE = snapshot.val();
 
-                  var strtoken = "Token is ";
-                  var stre = strtoken.concat(tokenRE);
-                  console.log(stre);
 
-                  var topic = "android";
-                  var payload = {
-                    data: {
-                      title: author,
-                      author: title
-                    },
-                    token: tokenRE
-
-                  };
-                  return admin
-                    .messaging()
-                    .send(payload)
-                    .then(function (response) {
-                      return console.log("Successfully sent message:", response);
-                    })
-                    .catch(function (error) {
-                      return console.log("Error sending message:", error);
-
-                    });
-                });
-
-            }),
-        }
-      })
-  })
+    });
+    
+  });
+>>>>>>> Stashed changes
